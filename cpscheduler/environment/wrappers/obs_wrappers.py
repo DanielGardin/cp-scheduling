b3221be7a8e6273dcd7ad27ebd46efa31a567220
Daniel Cluster <daniel.gratti.CW@eldorado.org.br>
2025-01-16 12:18:59 -0300

    fix: WrappedEnvs are now aligned to gym as ABCs

--- cpscheduler/environment/wrappers/obs_wrappers.py
+++ cpscheduler/environment/wrappers/obs_wrappers.py
@@ -10,11 +10,11 @@ from .. import Env
 _Act = TypeVar('_Act')
 class PytorchWrapper(WrappedEnv[Tensor, _Act]):
     def reset(self) -> tuple[Tensor, dict[str, Any]]:
-        obs, info = super().reset()
+        obs, info = self.env.reset()
 
         return as_tensor(obs), info
 
     def step(self, action: _Act, *args: Any, **kwargs: Any) -> tuple[Tensor, SupportsFloat, bool, bool, dict[str, Any]]:
-        obs, reward, terminated, truncated, info = super().step(action, *args, **kwargs)
+        obs, reward, terminated, truncated, info = self.env.step(action, *args, **kwargs)
 
         return as_tensor(obs), reward, terminated, truncated, info
