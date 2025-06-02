867460290f2c6e4c4a176f9f55c7857a0cc2cca9
Daniel Gardin <gardin.dan@gmail.com>
2025-06-02 17:41:47 +0000

    fix: WrappedEnvs are now aligned to gym as ABCs

--- cpscheduler/environment/wrappers/wrapper.py
+++ cpscheduler/environment/wrappers/wrapper.py
@@ -1,25 +1,26 @@
 from typing import Any, TypeVar, ParamSpec, SupportsFloat, Protocol
+from abc import ABC, abstractmethod
 
 from .. import Env
 
-_Obs = TypeVar('_Obs')
-_Act = TypeVar('_Act')
 _WrappedObs = TypeVar('_WrappedObs')
 _WrappedAct = TypeVar('_WrappedAct')
 _P = ParamSpec('_P')
-class WrappedEnv(Env[_WrappedObs, _WrappedAct]):
+class WrappedEnv(Env[_WrappedObs, _WrappedAct], ABC):
     def __init__(
             self,
-            env: Env[_Obs, _Act],
+            env: Env[Any, Any],
             *p_args: Any, **p_kwargs: Any
         ):
         self.env = env
 
+    @abstractmethod
     def reset(self) -> tuple[_WrappedObs, dict[str, Any]]:
-        raise NotImplementedError()
+        ...
 
+    @abstractmethod
     def step(self, action: _WrappedAct, *args: Any, **kwargs: Any) -> tuple[_WrappedObs, SupportsFloat, bool, bool, dict[str, Any]]:
-        raise NotImplementedError()
+        ...
 
     def render(self) -> None:
         self.env.render()
