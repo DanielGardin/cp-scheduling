bae2d1d3c7d41b971f6a8b28a71d3196aea3a04f
Daniel Gardin <gardin.dan@gmail.com>
2025-06-02 17:41:42 +0000

    feat: wrapper can now call step with args and kwargs

--- cpscheduler/environment/wrappers/wrapper.py
+++ cpscheduler/environment/wrappers/wrapper.py
@@ -11,18 +11,22 @@ _P = ParamSpec('_P')
 def wrap_obs(
         env: _Env,
         post_processor: Callable[Concatenate[Any, _P], _Obs],
-        *args: Any,
-        **kwargs: Any
+        *p_args: Any,
+        **p_kwargs: Any
     ) -> _Env:
     original_step = env.step
     original_reset = env.reset
-    def new_step(self: _Env, action: Any) -> tuple[_Obs, float, bool, bool, dict[str, Any]]:
-        obs, reward, terminated, truncated, info = original_step(action)
-        return post_processor(obs, *args, **kwargs), reward, terminated, truncated, info
+
+    def new_step(self: _Env, action: Any, *args: Any, **kwargs: Any) -> tuple[_Obs, float, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = original_step(action, *args, **kwargs)
+
+        return post_processor(obs, *p_args, **p_kwargs), reward, terminated, truncated, info
+
 
     def new_reset(self: _Env) -> tuple[_Obs, dict[str, Any]]:
         obs, info = original_reset()
-        return post_processor(obs, *args, **kwargs), info
+        return post_processor(obs, *p_args, **p_kwargs), info
+
 
     setattr(env, 'step', MethodType(new_step, env))
     setattr(env, 'reset', MethodType(new_reset, env))
@@ -33,14 +37,14 @@ def wrap_obs(
 def wrap_action(
         env: _Env,
         pre_processor: Callable[Concatenate[_Act, _P], Any],
-        *args: Any,
-        **kwargs: Any
+        *p_args: Any,
+        **p_kwargs: Any
     ) -> _Env:
     original_step = env.step
 
-    def new_step(self: _Env, action: _Act) -> tuple[Any, float, bool, bool, dict[str, Any]]:
-        new_action = pre_processor(action, *args, **kwargs)
-        return original_step(new_action)
+    def new_step(self: _Env, action: _Act, *args: Any, **kwargs: Any) -> tuple[Any, float, bool, bool, dict[str, Any]]:
+        new_action = pre_processor(action, *p_args, **p_kwargs)
+        return original_step(new_action, *args, **kwargs)
 
     setattr(env, 'step', MethodType(new_step, env))
 
