a52b698ed2f1b855e702116abfae84cd9a7451c4
Daniel Gardin <gardin.dan@gmail.com>
2025-06-02 17:41:45 +0000

    refactor: Now Env and VectorEnv are Generic Protocols

--- cpscheduler/environment/vector/__init__.py
+++ cpscheduler/environment/vector/__init__.py
@@ -1,30 +1,12 @@
-from typing import Any, Protocol, Iterable, Callable, SupportsFloat
-
-from .async_env import AsyncVectorEnv
-from .sync_env import SyncVectorEnv
-from .ray_env import RayVectorEnv
-
-from ..env import Env
-
-
-class VectorEnv(Protocol):
-    def __init__(
-            self,
-            env_fns: Iterable[Callable[[], Env]],
-            *args: Any, **kwargs: Any
-        ) -> None: ...
-
-    def reset(self) -> tuple[Any, dict[str, Any]]: ...
-
-    def step(self, actions: Iterable[Any], *args: Any, **kwargs: Any) -> tuple[list[Any], list[SupportsFloat], list[bool], list[bool], dict[str, Any]]: ...
-
-    def render(self) -> None: ...
-
-    def call(self, name: str, *args: Any, **kwargs: Any) -> tuple[Any, ...]: ...
-
 __all__ = [
+    'Env',
     'VectorEnv',
     'AsyncVectorEnv',
     'SyncVectorEnv',
     'RayVectorEnv'
 ]
+
+from .common import Env, VectorEnv
+from .sync_env import SyncVectorEnv
+from .async_env import AsyncVectorEnv
+from .ray_env import RayVectorEnv
