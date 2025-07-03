6533b72fa0939a44f1c3623afb09d283f7437104
Daniel Gardin <gardin.dan@gmail.com>
2025-07-03 17:16:32 -0300

    fix: improve error message for missing instances

--- tests/common.py
+++ tests/common.py
@@ -22,7 +22,16 @@ PROJECT_ROOT = Path(__file__).parent.parent
 def env_setup(instance_name: str) -> SchedulingEnv:
     path = PROJECT_ROOT / f"instances/jobshop/{instance_name}.txt"
 
-    instance, _ = read_jsp_instance(path)
+    try:
+        instance, _ = read_jsp_instance(path)
+    
+    except FileNotFoundError as e:
+        if not (path / "instances").exists():
+            raise FileNotFoundError(
+                f"Could not locate `instances` directory. Maybe you forgot to run `git submodule update --init`?"
+            )
+
+        raise e
 
     env = make(
         "Jobshop-v0",
