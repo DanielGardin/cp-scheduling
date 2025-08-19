bf420cd96c95a3030e3506d70d61b0a8bf1fd91d
Daniel Gardin <gardin.dan@gmail.com>
2025-08-19 13:36:49 -0300

    chore: Clean up, and removed explicit dependency of env on PlotlyRenderer

--- cpscheduler/__init__.py
+++ cpscheduler/__init__.py
@@ -1,18 +1,5 @@
 from .environment import *
 
-
-try:
-    from .gym import *
-
-except ImportError:
-    pass
-
-try:
-    from .solver import *
-
-except ImportError:
-    pass
-
 from .common import is_compiled
 
 __compiled__ = is_compiled()
