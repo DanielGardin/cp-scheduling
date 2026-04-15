f01b3a3bd11781c77d78bcadeddcc63586865df7
Daniel Gardin <gardin.dan@gmail.com>
2026-04-14 22:42:42 -0300

    feat: added constraints for optional tasks

--- cpscheduler/environment/constraints/optional.py
+++ cpscheduler/environment/constraints/optional.py
@@ -1,6 +1,13 @@
+from typing import Iterable
+
+from cpscheduler.environment.utils import convert_to_list
+from cpscheduler.environment.constants import Int, TaskID, MachineID
 from cpscheduler.environment.state import ScheduleState
 
-from cpscheduler.environment.constraints.base import PassiveConstraint
+from cpscheduler.environment.constraints.base import (
+    Constraint,
+    PassiveConstraint
+)
 
 
 class RejectableConstraint(PassiveConstraint):
@@ -16,3 +23,109 @@ class RejectableConstraint(PassiveConstraint):
 
     def get_entry(self) -> str:
         return "rej"
+
+class ExactlyOneConstraint(Constraint):
+    """
+    Alternative constraint where exactly one task have to be processed in each
+    one of the groups.
+    """
+
+    task_groups: list[list[TaskID]]
+
+    current_tasks: list[set[TaskID]]
+
+    def __init__(
+        self,
+        task_groups: Iterable[Iterable[Int]]
+    ) -> None:
+        self.task_groups = [
+            convert_to_list(tasks, TaskID)
+            for tasks in task_groups
+        ]
+
+        for tasks in self.task_groups:
+            if not tasks:
+                raise ValueError(
+                    "Cannot enforce exactly one constraint in a empty set of tasks."
+                )
+    
+    def reset(self, state: ScheduleState) -> None:
+        self.current_tasks = [
+            set(tasks) for tasks in self.task_groups
+        ]
+
+        for tasks in self.current_tasks:
+            if len(tasks) == 1:
+                remaining_task = next(iter(tasks))
+                state.require_task(remaining_task)
+                tasks.remove(remaining_task)
+
+    def on_presence(self, task_id: TaskID, state: ScheduleState) -> None:
+        for tasks in self.current_tasks:
+            if task_id not in tasks:
+                continue
+
+            tasks.remove(task_id)
+
+            for other_task in tasks:
+                state.forbid_task(other_task)
+
+    def on_assignment(
+        self, task_id: TaskID, machine_id: MachineID, state: ScheduleState
+    ) -> None:
+        self.on_presence(task_id, state)
+
+    def on_absence(self, task_id: TaskID, state: ScheduleState) -> None:
+        for tasks in self.current_tasks:
+            tasks.discard(task_id)
+
+            if len(tasks) == 1:
+                remaining_task = next(iter(tasks))
+                state.require_task(remaining_task)
+                tasks.remove(remaining_task)
+
+            elif len(tasks) == 0:
+                state.fail()
+
+class AtMostOneConstraint(Constraint):
+    """
+    Alternative constraint where at most one task have to be processed in each
+    one of the groups.
+    """
+
+    task_groups: list[list[TaskID]]
+
+    current_tasks: list[set[TaskID]]
+
+    def __init__(
+        self,
+        task_groups: Iterable[Iterable[Int]]
+    ) -> None:
+        self.task_groups = [
+            convert_to_list(tasks, TaskID)
+            for tasks in task_groups
+        ]
+    
+    def reset(self, state: ScheduleState) -> None:
+        self.current_tasks = [
+            set(tasks) for tasks in self.task_groups
+        ]
+    
+    def on_presence(self, task_id: TaskID, state: ScheduleState) -> None:
+        for tasks in self.current_tasks:
+            if task_id not in tasks:
+                continue
+
+            tasks.remove(task_id)
+
+            for other_task in tasks:
+                state.forbid_task(other_task)
+
+    def on_assignment(
+        self, task_id: TaskID, machine_id: MachineID, state: ScheduleState
+    ) -> None:
+        self.on_presence(task_id, state)
+    
+    def on_absence(self, task_id: TaskID, state: ScheduleState) -> None:
+        for tasks in self.current_tasks:
+            tasks.discard(task_id)
